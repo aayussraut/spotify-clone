ca7c474cd7b3e922beb06432b1ce8860b8bd0484
aayussraut <aayussraut.ar@gmail.com>
2023-05-14 14:18:10 +0545

    Refactored Home Page

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,58 +1,26 @@
-// import { BsFillPlayFill } from "react-icons/bs";
-// import {
-//   Button,
-//   Card,
-//   CardBody,
-//   CardSubtitle,
-//   CardTitle,
-//   Col,
-//   Row,
-// } from "reactstrap";
-// const HomepageCard = ({ title, subtitle, image }) => {
-//   return (
-//     <Card
-//       className="p-3 me-3 "
-//       style={{
-//         width: "196px",
-//         height: "286px",
-//         minWidth: "196px",
-//         backgroundColor: "#191919",
-//         overflow: "hidden",
-//       }}
-//     >
-//       <div className="position-relative">
-//         <img
-//           alt={title}
-//           src={image}
-//           className="card-img-top"
-//           style={{ height: "164px", width: "min-content" }} // 164px is the height of the card
-//         />
-//         <Button
-//           className="rounded-circle border-0 position-absolute bottom-0 end-0 playButton"
-//           style={{ width: "50px", height: "50px" }}
-//         >
-//           <BsFillPlayFill size={30} color="black" />
-//         </Button>
-//       </div>
-//       <CardBody className="p-0 pt-3">
-//         <CardTitle tag="h6" className="fw-bold text-white ">
-//           {title}
-//         </CardTitle>
-//         <CardSubtitle className="mb-2 text-muted" tag="h6">
-//           {subtitle}
-//         </CardSubtitle>
-//       </CardBody>
-//     </Card>
-//   );
-// };
-
 import { BsFillPlayFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
-import { Button, Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardTitle,
+  Col,
+  Container,
+  Row,
+} from "reactstrap";
+import { songAtom } from "../../recoil";
+import { useSetRecoilState } from "recoil";
 
-const HomepageCard = ({ title, subtitle, image }) => {
+const PlaylistCard = ({ item, roundedImage }) => {
+  const setPlaylist = useSetRecoilState(songAtom);
+  const handleSetSong = () => {
+    setPlaylist(item);
+  };
+
   return (
     <Card
+      onClick={handleSetSong}
       className="px-3 py-3"
       style={{
         width: "100%",
@@ -65,9 +33,9 @@ const HomepageCard = ({ title, subtitle, image }) => {
     >
       <div className="position-relative">
         <img
-          alt={title}
-          src={image}
-          className="card-img-top "
+          alt={item.name}
+          src={item.image_url}
+          className={`card-img-top ${roundedImage}`}
           style={{ height: "auto", width: "100%" }}
         />
         <Button
@@ -86,121 +54,108 @@ const HomepageCard = ({ title, subtitle, image }) => {
       </div>
       <CardBody className="p-0 pt-3">
         <CardTitle tag="h6" className="fw-bold text-white line-clamp">
-          {title}
+          {item.name}
         </CardTitle>
         <CardSubtitle className=" text-muted line-clamp" tag="h6">
-          {subtitle}
+          {item.artist || item.description || "Artist"}
         </CardSubtitle>
       </CardBody>
     </Card>
   );
 };
 
-// export { HomepageCard };
+const HomepageLongCard = ({ item }) => {
+  const setPlaylist = useSetRecoilState(songAtom);
+  const handleSetSong = () => {
+    setPlaylist(item);
+  };
 
-// import { BsFillPlayFill } from "react-icons/bs";
-// import {
-//   Button,
-//   Card,
-//   CardBody,
-//   CardSubtitle,
-//   CardTitle,
-//   Col,
-//   Container,
-//   Row,
-// } from "reactstrap";
-// const HomepageCard = ({ title, subtitle }) => {
-//   return (
-//     <Card
-//       className="p-3  card"
-//       style={{
-//         width: "196px",
-//         height: " 286px",
-//         backgroundColor: "#191919",
-//       }}
-//     >
-//       <Row className="position-relative m-0 p-0">
-//         <img
-//           alt="Sample"
-//           src="https://picsum.photos/300/200"
-//           className="p-0 "
-//           style={{ height: "160px" }}
-//         />
-//         <Button
-//           className="rounded-circle border-0 position-absolute bottom-0 end-0 playButton"
-//           style={{ width: "50px", height: "50px" }}
-//         >
-//           <BsFillPlayFill size={30} color="black" />
-//         </Button>
-//       </Row>
-//       <Row className="m-0 p-0">
-//         <CardBody className="p-0 pt-3">
-//           <CardTitle tag="h6" className="fw-bold text-white ">
-//             {title}
-//           </CardTitle>
-//           <CardSubtitle className="mb-2 text-muted" tag="h6">
-//             {subtitle}
-//           </CardSubtitle>
-//         </CardBody>
-//       </Row>
-//     </Card>
-//   );
-// };
+  return (
+    <Card
+      onClick={handleSetSong}
+      className="  justify-content-center longCard mb-3  p-0 m-0"
+      style={{
+        // maxWidth: "27.5rem",
+        height: "auto",
+        backgroundColor: "#191919",
+        overflow: "hidden",
+      }}
+    >
+      <Row
+        className=" align-items-center p-0 m-0  w-100"
+        style={{ width: "23rem" }}
+      >
+        <Col className="col-3 p-0 m-0  ">
+          <img
+            alt="item.name"
+            src={item.image_url}
+            className="card-img-top "
+            style={{ height: "5rem", width: "5rem" }}
+          />
+        </Col>
+        <Col className="col-7  position-relative   ">
+          <CardTitle tag="h6" className="fw-bold text-white line-clamp">
+            {item.name}
+          </CardTitle>
+        </Col>
+        <Col className=" d-flex col-2 m-0 p-0  ">
+          <Button
+            className="d-flex align-items-center rounded-circle playButton2"
+            style={{
+              width: "10vw",
+              height: "10vw",
+              minWidth: "50px",
+              minHeight: "50px",
+              maxWidth: "50px",
+              maxHeight: "50px",
+              backgroundColor: "#1ed661",
+            }}
+          >
+            <BsFillPlayFill size={30} color="black" />
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
 
-// export { HomepageCard };
+const SearchCard = ({ image_url, name }) => {
+  return (
+    <>
+      <Col className=" col-3 m-0 p-0">
+        <Container fluid>
+          <h2 className="fw-semibold" style={{ fontSize: "1.5rem" }}>
+            Top result
+          </h2>
+          <Card
+            className="p-4 w-auto h-auto"
+            // style={{ width: "372px", height: "237px" }}
+          >
+            <Row className=" m-0 row-4  p-0">
+              <div
+                className="border-0 rounded-circle p-0 m-0"
+                style={{ width: "92px", height: "92px" }}
+              >
+                <img
+                  src={image_url}
+                  className="rounded-circle"
+                  style={{ width: "92px", height: "92px" }}
+                />
+              </div>
+            </Row>
+            <Row className="m-0  row-4 ">
+              <h2 className="my-3 fw-bold p-0">{name}</h2>
+            </Row>
+            <Row className="m-0 row-4 m-">
+              <span className="border-0 fw-semibold bg-black rounded-5 w-auto">
+                Artist
+              </span>
+            </Row>
+          </Card>
+        </Container>
+      </Col>
+    </>
+  );
+};
 
-// import { BsFillPlayFill } from "react-icons/bs";
-// import {
-//   Button,
-//   Card,
-//   CardBody,
-//   CardSubtitle,
-//   CardTitle,
-//   Col,
-//   Container,
-//   Row,
-// } from "reactstrap";
-
-// const HomepageCard = ({ title, subtitle }) => {
-//   return (
-//     <Card
-//       className="p-3 mb-4 card"
-//       style={{
-//         width: "100%",
-//         height: "auto",
-//         backgroundColor: "#191919",
-//       }}
-//     >
-//       <Row className="position-relative m-0 p-0">
-//         <Col xs={12} className="p-0">
-//           <img
-//             alt="Sample"
-//             src="https://picsum.photos/300/200"
-//             className="w-100"
-//             style={{ height: "auto", objectFit: "cover" }}
-//           />
-//           <Button
-//             className="rounded-circle border-0 position-absolute bottom-0 end-0 translate-middle mb-3 me-3 playButton"
-//             style={{ width: "50px", height: "50px" }}
-//           >
-//             <BsFillPlayFill size={30} color="black" />
-//           </Button>
-//         </Col>
-//       </Row>
-//       <Row className="m-0 p-0">
-//         <Col className="p-0">
-//           <CardBody className="p-0 pt-3">
-//             <CardTitle tag="h6" className="fw-bold text-white ">
-//               {title}
-//             </CardTitle>
-//             <CardSubtitle className="mb-2 text-muted" tag="h6">
-//               {subtitle}
-//             </CardSubtitle>
-//           </CardBody>
-//         </Col>
-//       </Row>
-//     </Card>
-//   );
-// };
-
-export { HomepageCard };
+export { PlaylistCard, HomepageLongCard, SearchCard };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,190 +1,46 @@
 import { Col, Container, Row } from "reactstrap";
-import { HomepageCard } from "../component/Card/Card";
+import { HomepageLongCard } from "../component/Card/Card";
 import data from "../data/data.json";
-import HomeSection from "../component/Section/HomeSection";
-
+import { Link } from "react-router-dom";
+import { userAtom } from "../recoil";
+import { useRecoilValue } from "recoil";
+import PlayListSection from "../component/Section/PlaylistSection";
 const HomePage = () => {
+  const user = useRecoilValue(userAtom);
   return (
     <>
-      <HomeSection title="Albums" data={data.albums} />
-      <HomeSection title="Spotify Playlists" data={data.playlists} />
-      <HomeSection title="Focus" data={[...data.albums].reverse()} />
+      {user.user && (
+        <Container fluid className="px-4 mt-4">
+          <Row className=" no-gutters m-0 p-0  overflow-hidden ">
+            {data.playlists.slice(0, 6).map((item, index) => (
+              <>
+                <Col
+                  xs={6}
+                  sm={6}
+                  md={6}
+                  lg={6}
+                  xl={4}
+                  className=""
+                  key={index}
+                >
+                  <Link
+                    className="text-decoration-none"
+                    to={`/playlist/${item.id + "xyseffea235fe1"}`}
+                    state={{ data: item, itemId: item.id }}
+                  >
+                    <HomepageLongCard item={item} />
+                  </Link>
+                </Col>
+              </>
+            ))}
+          </Row>
+        </Container>
+      )}
+      <PlayListSection title="Albums" data={data.albums} />
+      <PlayListSection title="Spotify Playlists" data={data.playlists} />
+      <PlayListSection title="Focus" data={[...data.albums].reverse()} />
     </>
   );
 };
 
 export default HomePage;
-
-// const focusData = [
-//   {
-//     id: 1,
-//     title: "Peaceful Piano",
-//     subtitle: "Relax and indulge with beautiful piano pieces",
-//     img: "https://picsum.photos/300/200",
-//   },
-//   {
-//     id: 2,
-//     title: "Deep Focus",
-//     subtitle: "Keep calm and focus with ambient and post-rock music",
-//     img: "https://picsum.photos/300/200",
-//   },
-//   {
-//     id: 3,
-//     title: "Intense Studying",
-//     subtitle: "Music to help you concentrate",
-//     img: "https://picsum.photos/300/200",
-//   },
-//   {
-//     id: 4,
-//     title: "Beats to think to",
-//     subtitle: "Instrumental beats to help you think",
-//     img: "https://picsum.photos/300/200",
-//   },
-//   {
-//     id: 5,
-//     title: "Lo-Fi Beats",
-//     subtitle: "Beats to relax, study and focus",
-//     img: "https://picsum.photos/300/200",
-//   },
-//   {
-//     id: 6,
-//     title: "Chill Lofi Study Beats",
-//     subtitle: "Beats to relax, study and focus",
-//     img: "https://picsum.photos/300/200",
-//   },
-//   {
-//     id: 7,
-//     title: "Brain Food",
-//     subtitle: "Music for studying",
-//     img: "https://picsum.photos/300/200",
-//   },
-// ];
-
-// {
-//   /* <Container fluid className="px-4 mt-4">
-//         <div className="d-flex justify-content-between ">
-//           <h4 className="fw-bold">Focus</h4>
-//           <span>Show all</span>
-//         </div>
-//         <Row className="no-gutters m-0 p-0 flex-nowrap overflow-hidden ">
-//           {data.map((item, index) => (
-//             <Col
-//               xs={6}
-//               sm={6}
-//               md={4}
-//               lg={3}
-//               xl={2}
-//               className="my-3 gap-0"
-//               key={index}
-//             >
-//               <HomepageCard
-//                 title={item.albumName}
-//                 subtitle={item.artists}
-//                 image={item.coverImage}
-//               />
-//             </Col>
-//           ))}
-//         </Row>
-//       </Container> */
-// }
-// {
-//   /* <Container fluid className="p-0 m-0 px-4 mt-4 ">
-//         <div className="d-flex justify-content-between ">
-//           <h4 className="fw-bold">Spotify Playlists</h4>
-//           <span>Show all</span>
-//         </div> */
-// }
-// {
-//   /* <div className="d-flex p-0 m-0 ">
-//           {focusData.map((item) => (
-//             <HomepageCard
-//               key={item.id}
-//               title={item.title}
-//               subtitle={item.subtitle}
-//               //   img={item.img}
-//             />
-//           ))}
-//         </div> */
-// }
-// {
-//   /* <Container fluid className="p-0">
-//         <Row className="no-gutters m-0 p-0 flex-nowrap overflow-hidden ">
-//           {data.map((item, index) => (
-//             <HomepageCard
-//               key={index}
-//               title={item.albumName}
-//               subtitle={item.artists}
-//               //   img={item.img}
-//               image={item.coverImage}
-//             />
-//           ))}
-//         </Row>
-//       </Container> */
-// }
-// {
-//   /* <Container fluid className="p-0">
-//         <Row className="no-gutters m-0 p-0 flex-nowrap overflow-hidden ">
-//           {data.map((item, index) => (
-//             <Col
-//               xs={12}
-//               sm={6}
-//               md={4}
-//               lg={3}
-//               xl={2}
-//               className="my-3 gap-0"
-//               key={index}
-//             >
-//               <HomepageCard
-//                 title={item.albumName}
-//                 subtitle={item.artists}
-//                 image={item.coverImage}
-//               />
-//             </Col>
-//           ))}
-//         </Row>
-//       </Container>
-//       <Container fluid className="p-0">
-//         <Row
-//           className="no-gutters m-0 p-0 flex-nowrap overflow-hidden"
-//           gutter={2}
-//         >
-//           {data.map((item, index) => (
-//             <Col
-//               xs={12}
-//               sm={6}
-//               md={4}
-//               lg={3}
-//               xl={3}
-//               xxl={2}
-//               className="m-0 p-3"
-//               key={index}
-//             >
-//               <HomepageCard
-//                 title={item.albumName}
-//                 subtitle={item.artists}
-//                 image={item.coverImage}
-//               />
-//             </Col>
-//           ))}
-//         </Row> */
-// }
-// {
-//   /* </Container> */
-// }
-// {
-//   /* <Row className="d gy-4  p-0 m-0 ">
-//           {focusData.map((item) => (
-//             <Col className="p-0 gx-1">
-//               <HomepageCard
-//                 key={item.id}
-//                 title={item.title}
-//                 subtitle={item.subtitle}
-//                 //   img={item.img}
-//               />
-//             </Col>
-//           ))}
-//         </Row> */
-// }
-// {
-//   /* </Container> */
-// }
