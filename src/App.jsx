97d40250af7242e805e0f88e465900480bad77eb
aayussraut <aayussraut.ar@gmail.com>
2023-05-14 14:22:25 +0545

    Added Music bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Sidebar from "./component/Sidebar/Sidebar";
 import Header from "./component/Header/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./component/Footer/Footer";
+import MusicBar from "./component/Section/MusicBar";
+import { userAtom } from "./recoil";
+import { useRecoilValue } from "recoil";
 function App() {
+  const user = useRecoilValue(userAtom);
   return (
     <>
       <Container fluid className="p-0 m-0">
-        <Row className="m-0 p-0">
+        <Row className="m-0 p-0 ">
           <Col md={2} className="position-fixed m-0 p-0">
             <Sidebar />
           </Col>
@@ -17,6 +21,7 @@ function App() {
             <Outlet />
             <Footer />
           </Col>
+          {user.user && <MusicBar />}
         </Row>
       </Container>
     </>
